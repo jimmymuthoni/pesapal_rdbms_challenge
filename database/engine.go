fb390a90abaa9cc102669f5bef2eaca8a8b8bb2d
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-11 12:49:09 +0300

    feat: sql queries engine executor

---
+++ database/engine.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+var Schemas = make(map[string]TableSchema)
+
+// loading schema
+func LoadSchemas(){
+	file, err := os.ReadFile("schemas.json")
+	if err != nil {
+		return 
+	}
+	json.Unmarshal(file, &Schemas)
+}
+
+//save the schema
+func SaveSchemas(){
+	bytes, _ := json.MarshalIndent(Schemas,""," ")
+	os.WriteFile("schemas.json",bytes,0644)
+}
+
+func CreateTable(schema TableSchema){
+	Schemas[schema.Name] = schema
+	SaveSchemas()
+}
+
+func Insert(table string, row map[string]interface{}) error {
+	schema := Schemas[table]
+
+	rows, _ := ReadAllRows(table)
+	for _, r := range rows {
+		if r[schema.PrimaryKey] == row[schema.PrimaryKey]{
+			return errors.New("Primary key violation")
+		}
+	}
+	return InsertRow(table, row)
+}
+
+func SelectAll(table string) ([]map[string]interface{}, error){
+	return ReadAllRows(table)
+}
+
