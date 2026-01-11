4a061411af10a20ea2ca6ac0370339d018d9c50a
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-11 20:30:07 +0300

    feat: updated CRUD in sql engine

--- database/engine.go
+++ database/engine.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-var CurrentDM = "default"
+var CurrentDB = "default"
 var Schemas = make(map[string]TableSchema)
 
 // loading schema
@@ -25,10 +25,60 @@ func SaveSchemas(){
 	os.WriteFile("schemas.json",bytes,0644)
 }
 
+//function to create db
+func CreateDatabase(name string) error{
+	return os.MkdirAll("data/"+name, 0755)
+}
+
+//showing databases
+func ShowDatabases() ([]string, error){
+	files, err := os.ReadDir("data")
+	if err != nil {
+		return nil, err
+	}
+
+	var dbs []string
+	for _, f := range files {
+		if f.IsDir() {
+			dbs = append(dbs, f.Name())
+		}
+	}
+	return dbs,nil
+}
+
+//selcting database to use 
+func UseDatabase(name string) error {
+	if _, err := os.Stat("/data/" + name); err != nil {
+		return err
+	}
+	CurrentDB = name
+	return  nil
+}
+
+//creating table in slected database
 func CreateTable(schema TableSchema){
 	Schemas[schema.Name] = schema
 	SaveSchemas()
 }
+
+
+//showing the tables avaiable in database
+func ShowTables() ([]string, error){
+	path := "data/"+CurrentDB
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return  nil, err
+	}
+
+	var tables []string
+	for _, f := range files{
+		if !f.IsDir(){
+			tables = append(tables, f.Name())
+		}
+	}
+	return tables, nil
+}
+
 
 func Insert(table string, row map[string]interface{}) error {
 	schema := Schemas[table]
