5c40b9fb84df17f39f04b4cc230738bdcb291e6a
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-11 11:56:42 +0300

    feat: functions to read and insert rows in database implemented

---
+++ database/storage.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+	
+)
+
+//function to insert row to table in database
+func InsertRow(table string, row map[string]interface{}) error {
+	file, err := os.OpenFile("data/"+table+".tbl", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	bytes, _ := json.Marshal(row)
+	writer.Write(bytes)
+	writer.WriteString("\n")
+	return writer.Flush()
+
+}
+
+//function to read all rows in a table
+func ReadAllRows(table string) ([]map[string]interface{}, error){
+	file, err := os.Open("data/"+table+".tbl")
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var rows []map[string]interface{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		var row map[string]interface{}
+		json.Unmarshal(scanner.Bytes(), &row)
+		rows = append(rows, row)
+
+	}
+	return  rows, nil
+}
