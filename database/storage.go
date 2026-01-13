53dde00b3ad03650863c9c87b2824b8d8398296b
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-13 09:20:13 +0300

    feat: storage module complete

--- database/storage.go
+++ database/storage.go
@@ -9,7 +9,7 @@ import (
 
 //function to insert row to table in database
 func InsertRow(table string, row map[string]interface{}) error {
-	file, err := os.OpenFile("data/"+table+".tbl", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("data/" + CurrentDB + "/" + table + ".tbl", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
@@ -41,4 +41,21 @@ func ReadAllRows(table string) ([]map[string]interface{}, error){
 
 	}
 	return  rows, nil
+}
+
+
+func RewriteTable(table string, rows []map[string]interface{}) error {
+	file, err := os.Create("data/" + table+ ".tbl")
+	if err != nil {
+		return  err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, row := range rows {
+		bytes, _ := json.Marshal(row)
+		writer.Write(bytes)
+		writer.WriteString("\n")
+	}
+	return writer.Flush()
 }
